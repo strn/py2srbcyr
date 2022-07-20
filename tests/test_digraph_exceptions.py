b6286da97cfaac6d47e936882807e3e511c5dea6
Strn <strn@github.com>
2022-07-20 22:52:18 +0200

    Added more digraph exceptions

--- tests/test_digraph_exceptions.py
+++ tests/test_digraph_exceptions.py
@@ -9,4 +9,5 @@ class TestClass:
         self.cir = py2srbcyr.py2srbcyr.SerbCyr()
 
     def test_dj_exceptions(self):
-        assert self.cir.text_to_cyrillic('nadjačati odjednom') == 'над\u200cјачати од\u200cједном'
+        assert self.cir.text_to_cyrillic('nadjačati odjednom najdjelotvorniji') == \
+            'над\u200cјачати од\u200cједном најд\u200cјелотворнији'
